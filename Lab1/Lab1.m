ca64f1affb4dbbd89b1d129e51a2295d18a67b1c
jjv432 <jackvranicar43@gmail.com>
2024-09-06 09:47:10 -0400

    Working draw_robot animation

--- Lab1/Lab1.m
+++ Lab1/Lab1.m
@@ -6,5 +6,29 @@ format compact
 Box.x = 5;
 Box.y = 5;
 Box.phi = pi/4;
+Box.radius = 1;
+Box.width = 2;
+Box.length = 1;
 
-drawRobot(Box);
+Wheel.radius = .25;
+Wheel.wheel_width = 0.25;
+
+drawRobot(Box, Wheel);
+
+sample_length = 100;
+circle_radius = 3;
+
+x_vals = circle_radius * cos(linspace(0, 2*pi, sample_length));
+y_vals = circle_radius * sin(linspace(0, 2*pi, sample_length));
+
+%figure()
+Box.phi = pi/2;
+for i = 1:length(x_vals)
+    Box.x = x_vals(i);
+    Box.y = y_vals(i);
+    Box.phi = Box.phi + 2*pi/sample_length;
+    drawRobot(Box, Wheel);
+    pause(0)
+      
+
+end
--- Lab1/drawRobot.m
+++ Lab1/drawRobot.m
@@ -1,9 +1,13 @@
-function [] = drawRobot(Box)
+function [] = drawRobot(Box, Wheel)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
-x_box = [-1 -1 1 1 -1];
-y_box = [0.5 -0.5 -0.5 0.5 0.5];
+% All for main body
+length = Box.length; %y-direction
+width = Box.width; % x-direction
+
+y_box = [-length/2 -length/2 length/2 length/2 -length/2];
+x_box = [width/2 -width/2 -width/2 width/2 width/2];
 
 x = Box.x;
 y = Box.y;
@@ -14,10 +18,41 @@ box_rotated = rot_matrix * [x_box; y_box];
 
 box_translated_rotated = [box_rotated(1,:) + x; box_rotated(2,:) + y];
 
-figure()
+
+% Wheels
+radius = Wheel.radius;
+wheel_width = Wheel.wheel_width; %y direction
+
+% Left wheel
+x_left_wheel = [(-width/2 - radius), (-width/2 -radius), (-width/2 + radius), (-width/2 + radius), (-width/2 - radius)]; 
+y_left_wheel = [(length/2 + wheel_width), (length/2), (length/2), (length/2 + wheel_width), (length/2 + wheel_width)];
+
+left_wheel_rotated = rot_matrix * [x_left_wheel; y_left_wheel];
+
+left_wheel_rotated_translated = [left_wheel_rotated(1,:) + x; left_wheel_rotated(2,:) + y];
+
+% Right Wheel
+
+x_right_wheel = [(-width/2 - radius), (-width/2 -radius), (-width/2 + radius), (-width/2 + radius), (-width/2 - radius)]; 
+y_right_wheel = [(-length/2 - wheel_width), (-length/2), (-length/2), (-length/2 - wheel_width), (-length/2 - wheel_width)];
+
+right_wheel_rotated = rot_matrix * [x_right_wheel; y_right_wheel];
+
+right_wheel_rotated_translated = [right_wheel_rotated(1,:) + x; right_wheel_rotated(2,:) + y];
+
+%Plotting
+clf
+hold on
+
+
+    fill(right_wheel_rotated_translated(1,:), right_wheel_rotated_translated(2,:), 'k')
+    hold on
+    fill(left_wheel_rotated_translated(1,:), left_wheel_rotated_translated(2,:), 'k')
+    hold on
     fill(box_translated_rotated(1,:), box_translated_rotated(2,:), 'r');
     grid on
-    
+    axis equal
+    axis([-10 10 -10 10])
 
 
 end
