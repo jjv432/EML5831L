e3dafdc8df76d5e3f23dfdf41a4ace09068ff1f0
jjv432 <jackvranicar43@gmail.com>
2024-09-12 13:13:27 -0400

    End of lecture

---
+++ InClass091224/ParticleSwarm.m
@@ -0,0 +1,19 @@
+%% Particle Swarm Optimization
+
+N = 1000; %number of particles
+
+while(1)%termination condition is not met)
+    for i = 1:N
+        %Evaluate the cost
+        %update P_b ^ i (personal best for i)
+        %update G (global best)
+    end
+
+    %update alpha1
+
+    for i =1:N
+        %Compute velocity of each particle
+        %compute new position
+    end
+
+end
