ec5d4f9bda92500f7ee8841e30172b2bed0451fa
jjv432 <jackvranicar43@gmail.com>
2024-10-11 09:51:10 -0400

    Trajectories Updated

---
+++ Lab2/PointTurn.m
@@ -0,0 +1,20 @@
+clc
+clear
+close all
+format compact
+
+%%
+sample_length = 400;
+dt= 0.01;
+vx=0.2;
+x_vals=0;
+y_vals=0;
+phi(1)=0;
+
+for i = 2:sample_length
+     x_vals(i) = x_vals(i-1) + 0*vx*cos(phi(i-1))*dt;
+    y_vals(i) = y_vals(i-1) + 0*vx*sin(phi(i-1))*dt;
+    phi(i) = phi(i-1) + (2*pi)/sample_length;
+end
+
+trajectoryPlotter(x_vals, y_vals, phi);
