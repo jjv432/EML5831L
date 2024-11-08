c97083694f76f4743a40a30761ef8e38dc2c9802
jjv432 <jackvranicar43@gmail.com>
2024-11-08 10:31:17 -0500

    Report version

--- Lab4/01-wall-follower/main_3.m
+++ Lab4/01-wall-follower/main_3.m
@@ -2,38 +2,47 @@ clc; close all; format compact;
 
 addpath('../rc-labs/rc-matlab-lib');
 
+% Only need to run once, just don't clearvars
 %RC = RCCar();
 
+% Put the robot at the origin, clear map
 resetMap(RC);
 
 %Second dimension is left and right
-Waypoint = [.5 0];
+Waypoint = [.5 0]; % m
 
-threshold = .05;
+% How close the robot needs to be to the waypoint to be considered
+% succcessful
+threshold = .05; % m
 
+% Initially, the robot is stopped with no steering angle
 RC.setSteeringAngle(0);
 RC.setSpeed(0);
 
+% while the robot isn't close enough, get closer to it
 while sqrt((Waypoint(1) - RC.X)^2 + (Waypoint(2) - RC.Y)^2) > threshold
 
     phi = RC.Phi;
-
+    
+    % finding WP coords in the robot frame
     RotMatrix = [cos(phi) sin(phi); -sin(phi) cos(phi)];
-
     local = RotMatrix * [(Waypoint(1) - RC.X); (Waypoint(2) -RC.Y)];
-
+    
+    % Turn radius calculations
     l_squared = local(1)^2 + local(2)^2;
-
     radius = l_squared / (2 * local(2));
 
+    % Angle from the robot x axis to the WP
     localPhi = atan2(local(2), local(1));
-
-    gamma = radius * (.25/.715);
+    
+    % Experimentally determined relationship between gamma and radius
+    gamma = radius * (.25/.715); 
 
     % If it needs to steer too hard, back up a bit to make the turn less
     % brutal
-    localPhi * 180/pi
-
+    
+    % if the waypoint is outside of a 'cone' in front of the robot, back up
+    % in a straight line
     if localPhi < -pi/4
         RC.setSpeed(-.25);
         RC.setSteeringAngle(0)
@@ -45,6 +54,8 @@ while sqrt((Waypoint(1) - RC.X)^2 + (Waypoint(2) - RC.Y)^2) > threshold
         RC.setSteeringAngle(gamma)
     end
 
+    % Making sure steering angle doesn't get too large (past physical
+    % limitation)
     if gamma > .25
         RC.setSteeringAngle(.25);
 
@@ -52,25 +63,11 @@ while sqrt((Waypoint(1) - RC.X)^2 + (Waypoint(2) - RC.Y)^2) > threshold
         RC.setSteeringAngle(-.25);
     end
 
-    % if gamma > .25
-    %     RC.setSteeringAngle(-.25)
-    %     RC.setSpeed(-.25);
-    % elseif gamma < -.25
-    %     RC.setSteeringAngle(.25)
-    %     RC.setSpeed(.25);
-    % else
-    %     RC.setSpeed(.25);
-    %     RC.setSteeringAngle(gamma);
-    % end
-    %
-    % if local(1) > 0
-    %     gamma = -gamma;
-    % end
-
     pause(.05);
 
 end
 
+% Turn the robot 'off'
 RC.setSteeringAngle(0);
 RC.setSpeed(0);
 
