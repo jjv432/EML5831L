f4a55d0261c782b5318855ee3a608187c4a8eaa7
jjv432 <jackvranicar43@gmail.com>
2024-12-04 14:31:02 -0500

    RRT works with saved map

--- Lab6/checkPoint.m
+++ Lab6/checkPoint.m
@@ -1,8 +1,5 @@
 function [inPolygon, newX, newY] = checkPoint(newPoint, beginningPoint, saved_map, phi, lookahead, radius_min)
 
-% val = checkOccupancy(saved_map, [check_x check_y]);
-
-%vobstacles
 inSum = 0;
 searchRes = 100;
 
@@ -22,40 +19,12 @@ else
     inPolygon = 0;
 end
 
-% turn radius fixing
-
-rotMatrix = [cos(phi) sin(phi); -sin(phi) cos(phi)];
-deltaX = newPoint(1) - beginningPoint(1);
-deltaY = newPoint(2) - beginningPoint(2);
-
-newCoords = rotMatrix*[deltaX; deltaY];
-
-xl = newCoords(1);
-yl = newCoords(2);
-
-turn_radius = lookahead^2 / 2 * yl;
-
-% only change the new waypoint if it doesnt fit on a turn radius
-if turn_radius < radius_min
-
-    % something about this math is wrong
-    turn_radius = radius_min;
-    yl = lookahead^2 / (2 * turn_radius) + beginningPoint(2);
-    xl = sqrt(lookahead^2 - yl^2) + beginningPoint(1);
-
-    newGlobal = rotMatrix' * [xl; yl];
-
-    % newX = newGlobal(1);
-    % newY = newGlobal(2);
-
+if inSum > 0
+    inPolygon = 1;
 else
-    % newX = newPoint(1);
-    % newY = newPoint(2);
+    inPolygon = 0;
 end
 
-% This format works, it's just the calculations above that are wrong
 newX = newPoint(1);
 newY = newPoint(2);
-
-
 end
--- Lab6/main.m
+++ Lab6/main.m
@@ -157,3 +157,54 @@ for i = 1:length(indices)
     drawnow;
 end
 % Plot your results on the saved map
+
+%% Purepursuit
+threshold = 0.05; % Threshold to determine if the robot has reached the waypoint
+RC.setSpeed(0); % Reset any motion before starting
+RC.setSteeringAngle(0);
+
+for idx = 1:length(indices) - 1
+    % Get the current and next waypoint
+    Waypoint = [NodeList(1, indices(idx + 1)), NodeList(2, indices(idx + 1))];
+
+    while sqrt((Waypoint(1) - RC.X)^2 + (Waypoint(2) - RC.Y)^2) > threshold
+        phi = RC.Phi;
+        
+        % Transform waypoint to the robot's frame
+        RotMatrix = [cos(phi), sin(phi); -sin(phi), cos(phi)];
+        local = RotMatrix * [(Waypoint(1) - RC.X); (Waypoint(2) - RC.Y)];
+        
+        % Turn radius calculations
+        l_squared = local(1)^2 + local(2)^2;
+        if local(2) ~= 0
+            radius = l_squared / (2 * local(2));
+        else
+            radius = inf; % If straight ahead, assume large radius
+        end
+        
+        % Calculate steering angle (gamma)
+        gamma = radius * (0.25 / 0.715); % Adjust constants based on your robot's dynamics
+        
+        % Determine motion based on waypoint position in the robot's frame
+        localPhi = atan2(local(2), local(1));
+        if localPhi < -pi/4
+            % Too far left, reverse slightly
+            RC.setSpeed(-0.25);
+            RC.setSteeringAngle(0);
+        elseif localPhi > pi/4
+            % Too far right, reverse slightly
+            RC.setSpeed(-0.25);
+            RC.setSteeringAngle(0);
+        else
+            % Forward motion towards waypoint
+            RC.setSpeed(0.25);
+            RC.setSteeringAngle(min(max(gamma, -0.25), 0.25)); % Constrain steering angle
+        end
+
+        pause(0.05); % Small delay for command execution
+    end
+end
+
+% Stop the robot at the end
+RC.setSteeringAngle(0);
+RC.setSpeed(0);
